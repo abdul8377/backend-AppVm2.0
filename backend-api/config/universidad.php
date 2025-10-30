436e5f5943063f490ff5a83ac0c160b0c1770c3a
abdul8377 <abdulalfonso77@gmail.com>
2025-10-30 10:04:33 -0500

    first commit

--- backend-api/config/universidad.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    // Universidad fija con la que operan los endpoints de Sede
-    'fixed_id' => 1,
-];
--- backend-api/tests/Pest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-
-uses(Tests\TestCase::class, RefreshDatabase::class)->in('Feature');
-uses(Tests\TestCase::class)->in('Unit');
